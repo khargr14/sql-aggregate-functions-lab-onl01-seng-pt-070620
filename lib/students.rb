ff30fc65df824c2f53a51e600ccb691bc0cc4caf
Keesha Hargrove <kharg14@gmail.com>
2020-09-07 12:45:20 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,19 +1,6 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
-# def highest_student_gpa
-# end
 
-# def lowest_student_gpa
-# end
-
-# def average_student_gpa
-# end
-
-# def total_tardies_for_all_students
-# end
-
-# def average_gpa_for_9th_grade
-# end
 
 def highest_student_gpa
     "SELECT gpa FROM students ORDER BY gpa DESC LIMIT 1";
